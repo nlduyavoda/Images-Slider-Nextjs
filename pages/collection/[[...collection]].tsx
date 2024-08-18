1de22bbca321a7d9fe8d6c124d03ae1dae8c31de
Le Duy <nguyenleduy1199@gmail.com>
2024-08-18 19:48:22 +0700

    change column-count

--- pages/collection/[[...collection]].tsx
+++ pages/collection/[[...collection]].tsx
@@ -34,7 +34,7 @@ const Collection = () => {
 }
 
 export const Container = styled.div`
-  column-count: 4;
+  column-count: 5;
   column-gap: 20px 20px;
   @media (max-width: 1024px) {
     column-count: 3;
